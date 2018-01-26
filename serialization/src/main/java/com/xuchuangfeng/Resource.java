e4ea835ae9758ef8d696936e320767647509f6d6
Barry C F Xu <417831744@qq.com>
2018-01-26 12:04:48 +0800

    2018年1月26日12:04:34 - serialization

---
+++ serialization/src/main/java/com/xuchuangfeng/Resource.java
@@ -0,0 +1,30 @@
+package com.xuchuangfeng;
+
+import com.xuchuangfeng.core.Serializer;
+
+/**
+ * @author XuChuangFeng
+ */
+public class Resource extends Serializer {
+
+    private int gold;
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    @Override
+    protected void read() {
+        this.gold = readInt();
+    }
+
+    @Override
+    protected void write() {
+        writeInt(gold);
+    }
+
+}
---
+++ serialization/src/main/java/com/xuchuangfeng/Test3.java
@@ -0,0 +1,32 @@
+package com.xuchuangfeng;
+
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
+
+import java.util.Arrays;
+
+/**
+ * @author XuChuangFeng
+ */
+public class Test3 {
+
+    public static void main(String[] args) {
+
+        ChannelBuffer buffer = ChannelBuffers.dynamicBuffer();
+        buffer.writeInt(101);
+        buffer.writeDouble(80.1);
+
+        byte[] bytes = new byte[buffer.writerIndex()];
+        buffer.readBytes(bytes);
+
+        System.out.println(Arrays.toString(bytes));
+
+        "abc".getBytes();
+
+        //================================================
+        ChannelBuffer wrappedBuffer = ChannelBuffers.wrappedBuffer(bytes);
+        System.out.println(wrappedBuffer.readInt());
+        System.out.println(wrappedBuffer.readDouble());
+    }
+
+}
